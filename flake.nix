d4d01125e279dd5a29c1df60a9c463efef8d0ba0
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-10-24 09:23:22 -0500

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "My Nix Flakes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    snowfall-lib = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:snowfallorg/lib";
+    };
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs: let
+    lib = inputs.snowfall-lib.mkLib {
+      inherit inputs;
+      src = ./.;
+
+      snowfall = {
+        meta = {
+          name = "ironman-apps";
+          title = "Ironman Apps";
+        };
+        namespace = "ironman";
+      };
+    };
+  in lib.mkFlake {
+    channels-config = {
+      allowUnfree = true;
+    };
+
+    # overlays = with inputs; [
+    # ];
+
+    alias = {
+      shells.default = "ironman-shell";
+    };
+  };
+}
